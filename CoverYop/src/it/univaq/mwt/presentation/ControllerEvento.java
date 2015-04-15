8ee472882d0096a2a0a67f466b262be2a6fdc09f
Ciufillo <matteo.comignani@gmail.com>
2015-04-15 20:29:39 +0200

    primo commit matteo

--- CoverYop/src/it/univaq/mwt/presentation/ControllerEvento.java
+++ CoverYop/src/it/univaq/mwt/presentation/ControllerEvento.java
@@ -31,7 +31,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+////////////ciao
 
 
 @Controller
