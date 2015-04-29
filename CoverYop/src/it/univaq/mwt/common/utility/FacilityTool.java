439f4db74d7c14d62dcdc550baf97ffd0bf2353b
unknown <giona.granchelli@gmail.com>
2015-04-29 16:38:52 +0200

    Facility Tool, Split Name

--- CoverYop/src/it/univaq/mwt/common/utility/FacilityTool.java
+++ CoverYop/src/it/univaq/mwt/common/utility/FacilityTool.java
@@ -1,12 +1,39 @@
 package it.univaq.mwt.common.utility;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.univaq.mwt.business.RuoloService;
+import it.univaq.mwt.business.model.Gruppo;
+import it.univaq.mwt.business.model.Locale;
+import it.univaq.mwt.business.model.Ruolo;
+import it.univaq.mwt.business.model.Utente;
+
 public final class FacilityTool {
 
-public static String[] splitName(String nome){
-		
+	
+	public static String[] splitName(String nome){
 		String[] title = nome.split(" ");
-		
 		return title;
+	}
+	
+	public static Utente finalizeUserInfo(Utente user, String tipo, RuoloService rs){
+		
+		String address = user.getCitta()+", "+user.getIndirizzo();
+		String latLongs[];
+		try {
+			latLongs = ConversionUtility.getLatLongPositions(address);
+			float lat = Float.parseFloat(latLongs[0]);
+			float lng = Float.parseFloat(latLongs[1]);
+			user.setLat(lat);
+			user.setLng(lng);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Ruolo role = rs.getRuoloByName(tipo);
+		user.setRuolo(role);
+		
+		return user;
 		
 	}
 }
